aa68e4c0896c414223ffd050cf1fb89647bc03aa
qsmith4080 <qsmith6440@gmail.com>
2022-06-06 21:42:42 -0400

    Add files via upload

---
+++ hw03/src/main/java/LinkedDeque.java
@@ -0,0 +1,187 @@
+import java.lang.IllegalArgumentException;
+import java.util.NoSuchElementException;
+
+/**
+ * Your implementation of a LinkedDeque.
+ *
+ * @author Quincy Smith
+ * @version 1.0
+ * @userid qsmith35
+ * @GTID 903408775
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+public class LinkedDeque<T> {
+
+    // Do not add new instance variables or modify existing ones.
+    private LinkedNode<T> head;
+    private LinkedNode<T> tail;
+    private int size;
+
+    // Do not add a constructor.
+
+    /**
+     * Adds the element to the front of the deque.
+     *
+     * Must be O(1).
+     *
+     * @param data the data to add to the front of the deque
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void addFirst(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Deque does not "
+            + "accept null elements");
+        }
+
+        if (size == 0) {
+            head = new LinkedNode<>(data);
+            tail = head;
+        } else {
+            head.setPrevious(new LinkedNode<>(data, null, head));
+            head = head.getPrevious();
+        }
+
+        size++;
+    }
+
+    /**
+     * Adds the element to the back of the deque.
+     *
+     * Must be O(1).
+     *
+     * @param data the data to add to the back of the deque
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void addLast(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Deque does not "
+            + "accept null data");
+        }
+
+        if (size == 0) {
+            head = new LinkedNode<>(data);
+            tail = head;
+        } else {
+            tail.setNext(new LinkedNode<>(data, tail, null));
+            tail = tail.getNext();
+        }
+
+        size++;
+    }
+
+    /**
+     * Removes and returns the first element of the deque.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the front of the deque
+     * @throws java.util.NoSuchElementException if the deque is empty
+     */
+    public T removeFirst() {
+        if (size == 0) {
+            throw new NoSuchElementException("Deque is empty");
+        }
+
+        LinkedNode<T> deletedNode = head;
+        head = head.getNext();
+        head.setPrevious(null);
+        size--;
+
+        return deletedNode.getData();
+    }
+
+    /**
+     * Removes and returns the last element of the deque.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the back of the deque
+     * @throws java.util.NoSuchElementException if the deque is empty
+     */
+    public T removeLast() {
+        if (size == 0) {
+            throw new NoSuchElementException("Deque is empty");
+        }
+
+        LinkedNode<T> deletedNode = tail;
+        tail = tail.getPrevious();
+        tail.setNext(null);
+        size--;
+
+        return deletedNode.getData();
+    }
+
+    /**
+     * Returns the first data of the deque without removing it.
+     *
+     * Must be O(1).
+     *
+     * @return the data located at the front of the deque
+     * @throws java.util.NoSuchElementException if the deque is empty
+     */
+    public T getFirst() {
+        if (size == 0) {
+            throw new NoSuchElementException("Deque is empty");
+        }
+
+        return head.getData();
+    }
+
+    /**
+     * Returns the last data of the deque without removing it.
+     *
+     * Must be O(1).
+     *
+     * @return the data located at the back of the deque
+     * @throws java.util.NoSuchElementException if the deque is empty
+     */
+    public T getLast() {
+        if (size == 0) {
+            throw new NoSuchElementException("Deque is empty");
+        }
+
+        return tail.getData();
+    }
+
+    /**
+     * Returns the head node of the deque.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return node at the head of the deque
+     */
+    public LinkedNode<T> getHead() {
+        // DO NOT MODIFY THIS METHOD!
+        return head;
+    }
+
+    /**
+     * Returns the tail node of the deque.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return node at the head of the deque
+     */
+    public LinkedNode<T> getTail() {
+        // DO NOT MODIFY THIS METHOD!
+        return tail;
+    }
+
+    /**
+     * Returns the size of the deque.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the deque
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+}
